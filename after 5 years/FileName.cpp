cee002b184c196d0077a1b0eb9a58880466cce64
mvrwu <mvrwu@JYEEN>
2025-08-14 16:55:39 +0000

    Add project files.

---
+++ after 5 years/FileName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int Age1;
+	int Age2; 
+	
+	cout << "Enter the first age: " << endl; 
+	cin >> Age1;
+
+	cout << "Enter the second age: " << endl; 
+	cin >> Age2; 
+
+	cout << "After " << Age1 << " years you will be " << Age2 << " years old.";
+}
